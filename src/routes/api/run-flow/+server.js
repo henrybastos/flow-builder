286be8023f44b833de81fcd5b4ef888b95dd0bf4
Henry B Silva <hb.personal.ctt@gmail.com>
2023-11-27 17:38:33 -0300

    Fix: Header upgrade-insecure-requests breaking some login pages

--- src/routes/api/run-flow/+server.js
+++ src/routes/api/run-flow/+server.js
@@ -64,8 +64,7 @@ export async function POST ({ request }) {
         });
 
         await page.setExtraHTTPHeaders({ 
-            'user-agent': 'Chrome/108.0.0.0', 
-            'upgrade-insecure-requests': '1', 
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)  Safari/537.36', 
             'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8', 
             'accept-encoding': 'gzip, deflate, br', 
             'accept-language': 'en-US,en;q=0.9,en;q=0.8' 
