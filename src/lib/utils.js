4041f6a39cc31c7fadfb4791e73491d36839d493
marketaDevHenry <hb.personal.ctt@gmail.com>
2023-11-29 00:13:11 -0300

    Broken shit T _ T

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,143 +3,11 @@ const envPlaceholder = {
     end: '%'
 }
 
+const globalEnvPlaceholder = "\\$\\$";
+
 const placeholderMatchRegExp = new RegExp(`${ envPlaceholder.start }.*${ envPlaceholder.end }`, 'g');
 const placeholderReplaceRegExp = new RegExp(`(${ envPlaceholder.start }|${ envPlaceholder.end })`, 'g');
-
-export function convertInputsToKronusJson (_structure_type, _obj, _structure) {
-    let buffer = [];
-
-    switch (_structure_type) {
-        case 'course_copy':
-            for (let key of Object.keys(_obj)) {
-                switch (key) {
-                    case 'how_items':
-                        buffer.push(...courseConversionTransforms.how_items( _structure[key], _obj[key].items ));
-                        break;
-                    case 'bonus':
-                        buffer.push(...courseConversionTransforms.bonus( _structure[key], _obj[key].items ));
-                        break;
-                    case 'options':
-                        buffer.push(...courseConversionTransforms.options( _structure[key], _obj[key].items ));
-                        break;
-                    case 'faq':
-                        buffer.push(...courseConversionTransforms.faq( _structure[key], _obj[key].items ).flat());
-                        break;
-                    default:
-                        buffer.push({
-                            "key": _structure[key],
-                            "value": `\n${ _obj[key].value.trim() }\n`
-                        });
-                        break;
-                }
-            }
-            
-            buffer.push(
-                {
-                    "key": "301",
-                    "value": "\nSobre o Curso\n"
-                }
-            );
-            break;
-        case 'expert_copy':
-            for (let key of Object.keys(_obj)) {
-                console.log(`Key: ${ key }`);
-                
-                switch (key) {
-                    case 'specialty':
-                        buffer.push(expertConversionTransforms.specialty( _structure[key], _obj[key].value.trim() ));
-                        break;
-                    case 'full_description':
-                        buffer.push(expertConversionTransforms.full_description( _structure[key], _obj[key].value.trim() ));
-                        break;
-                    case 'link_selling':
-                        buffer.push(expertConversionTransforms.link_selling( _structure[key], _obj[key].value.trim() ));
-                        break;
-                    default:
-                        buffer.push({
-                            "key": _structure[key],
-                            "value": `\n${ _obj[key].value.trim() }\n`
-                        });
-                        break;
-                }
-            }
-            break;
-    }
-
-    return buffer;
-}
-
-const expertConversionTransforms = {
-    full_description: (_key, _value) => {
-        return {
-            "key": _key,
-            "value": `\nDescrição Completa:\n${ _value.trim() }\n`
-        }
-    },
-    specialty: (_key, _value) => {
-        return {
-            "key": _key,
-            "value": `\nEspecialidade:\n${ _value.trim() }\n`
-        }
-    },
-    link_selling: (_key, _value) => {
-        return {
-            "key": _key,
-            "value": `\nUrl do Site:\n${ _value.trim() }\n`
-        }
-    }
-}
-
-const courseConversionTransforms = {
-    how_items: (_keys, _items) => {
-        let howItems = _items.map((_item, _i) => {
-            return {
-                key: _keys[_i],
-                value: `\n${ _item.title.value.trim() }\n${ _item.description.value.trim() }\n`
-            }
-        })
-        return howItems;
-    },
-    bonus: (_keys, _items) => {
-        let bonusItems = _items.map((_item, _i) => {
-            return {
-                key: _keys[_i],
-                value: `\nBônus ${ _i + 1 }\nTítulo:\n${ _item.title.value.trim() }\nDescrição:\n${ _item.description.value.trim() }\nPreço:\n${ _item.price.value.trim() }\nHeadlines:\n${ _item.headline_1.value.trim() }\n${ _item.headline_2.value.trim() }\n${ _item.headline_3.value.trim() }\n`
-            }
-        })
-        return bonusItems;
-    },
-    options: (_keys, _items) => {
-        let optionsItems = _items.map((_item, _i) => {
-            return {
-                key: _keys[_i],
-                value: `\n${ _item.option.value.trim() }\n`
-            }
-        })
-        return optionsItems;
-    },
-    faq: (_keys, _items) => {
-        let faqIndex = 0;
-        
-        let faqItems = _items.map((_item) => {
-            const question = {
-                key: _keys[faqIndex],
-                value: `\n${ _item.title.value.trim() }\n`
-            };
-
-            const answer = {
-                key: _keys[faqIndex + 1],
-                value: `\n${ _item.answer.value.trim() }\n`
-            };
-
-            faqIndex += 2;
-            
-            return [ question, answer ];
-        });
-
-        return faqItems;
-    }
-};
+const globalPlaceholderMatchRegExp = new RegExp(`^${ globalEnvPlaceholder }env.`, 'g');
 
 export function filterKeys (_obj, _query) {
     for (let key of Object.keys(_obj)) {
@@ -205,8 +73,19 @@ export function checkForEnvPlaceholder (_str) {
     return _str.match(placeholderMatchRegExp);
 }
 
+export function checkForGlobalEnvPlaceholder (_str) {
+    return _str.match(globalPlaceholderMatchRegExp);
+    // const trimmed_str = trimEnvPlaceholder(_str).replace(globalPlaceholderMatchRegExp, '');
+    // console.log(`[TRIMMED]: ${ trimmed_str }`);
+    // if (trimmed_str) {
+    //     checkEnvVars(_env.match(new RegExp(`(?<=${ globalEnvPlaceholder }env\.).*`, 'g')[0]), trimmed_str)
+    // } else {
+    //     return null;
+    // }
+}
+
 export function trimEnvPlaceholder (_str) {
-    return _str.match(placeholderMatchRegExp)[0].replaceAll(placeholderReplaceRegExp, '');
+    return _str.match(placeholderMatchRegExp)[0].replaceAll(placeholderReplaceRegExp, '').replace(globalPlaceholderMatchRegExp, '');
 }
 
 export function resolveDotNotation (_obj, _str) {
@@ -216,6 +95,14 @@ export function resolveDotNotation (_obj, _str) {
 export function replaceEnvPlaceholder (_str, _env) {
     let envVariable = resolveDotNotation(_env, trimEnvPlaceholder(_str));
     return envVariable.match(placeholderMatchRegExp) ? replaceEnvPlaceholder(envVariable, _env) : envVariable;
+}
+
+export function checkEnvVars (_env, _field_value) {
+    if (checkForEnvPlaceholder(_field_value)) {
+        return replaceEnvPlaceholder(_field_value, _env);
+    }
+
+    return _field_value;
 }
 
 export function genUUID () {
