d71675d983712e8d339fdb1edbfd8177c6a1defd
marketaDevHenry <hb.personal.ctt@gmail.com>
2023-11-30 23:46:05 -0300

    fix(EnvHandler.js): Some fixes and some refactor.

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -3,59 +3,7 @@ const envPlaceholder = {
     end: '%'
 }
 
-const globalEnvPlaceholder = "\\$\\$";
-
 export const placeholderMatchRegExp = new RegExp(`${ envPlaceholder.start }.*${ envPlaceholder.end }`, 'g');
-export const placeholderReplaceRegExp = new RegExp(`(${ envPlaceholder.start }|${ envPlaceholder.end })`, 'g');
-export const globalPlaceholderMatchRegExp = new RegExp(`${ globalEnvPlaceholder }env.`, 'g');
-
-export function filterKeys (_obj, _query) {
-    for (let key of Object.keys(_obj)) {
-        if (_query.includes(key) && key !== 'value') {
-            // console.log(`Forbidden key: ${ key }`);
-            delete _obj[key];
-        } else {
-            if (typeof _obj[key] === 'object') {
-                filterKeys(_obj[key], _query);
-            }
-        }
-    }
-}
-
-// Returns an object with only the desired keys.
-export function filterDesiredKeys (_obj, _keys) {
-    let clone = structuredClone(_obj);
-
-    for (let key of Object.keys(clone)) {
-        if (_keys.includes(key)) {
-            if (typeof key === 'object') {
-                clone = filterObject(key, _keys);
-            }
-        } else {
-            delete clone[key];
-        }
-    }
-    return clone;
-}
-
-export function deleteEmptyObjs (_obj) {
-    for (let key of Object.keys(_obj)) {
-        if (typeof _obj[key] === 'object') {
-            if (Object.keys(_obj[key]).length === 0) {
-                delete _obj[key];
-            } else {
-                deleteEmptyObjs(_obj[key]);
-            }
-        }
-    }
-}
-
-export function filterObject (_obj, _query) {
-    let _buffer = structuredClone(_obj);
-    filterKeys(_buffer, _query);
-    deleteEmptyObjs(_buffer);
-    return _buffer;
-}
 
 export function snakeCaseToPascalCase (_string, _spaced = false) {
     if (_spaced) {
@@ -67,19 +15,6 @@ export function snakeCaseToPascalCase (_string, _spaced = false) {
 
 export function convertToSnakeCase (_string) {
     return _string.toLowerCase().replaceAll(/[\s-]/g, '_');
-}
-
-// checkEnvVars, checkForGlobalEnvPlaceholder, checkForEnvPlaceholder
-
-export function checkEnvVars (_field_value, _env) {
-    let value = _field_value.match(placeholderMatchRegExp)[0].replace(globalPlaceholderMatchRegExp, '');
-    // console.log(_field_value, value, value.replaceAll(placeholderReplaceRegExp, ''), _env);
-    
-    if (value.match(placeholderMatchRegExp)) {
-        return [_env, ...value.replaceAll(placeholderReplaceRegExp, '').split('.')].reduce((a,b) => a[b]);
-    }
-
-    return _field_value;
 }
 
 export function genUUID () {
