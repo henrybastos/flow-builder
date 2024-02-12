20f3bf88618301fce92970b4a6b0a33b0a688bcb
Henry B Silva <henrybastoscomercial@gmail.com>
2024-02-12 10:39:57 -0300

    Flow Blocks to gitignore file

--- src/lib/flow-blocks/FlowBlocks.ts
+++ src/lib/flow-blocks/FlowBlocks.ts
@@ -1,15 +1,5 @@
-import { EduzzLoginBlock } from "./EduzzLoginBlock";
-import { EduzzCreateMultipleCouponsBlock } from "./EduzzCreateMultipleCouponsBlock";
-import { MyNutrorCreateModulesAndLessonsBlock } from "./MyNutrorCreateModulesAndLessonsBlock";
-import { GoogleAnalyticsCreatePropertyBlock } from "./GoogleAnalyticsCreatePropertyBlock";
-import { JivoCreateChannelBlock } from "./JivoCreateChannelBlock";
 import { YTEvalDurationScraperConverterBlock } from "./YTEvalDurationScraperConverterBlock";
 
 export const FlowBlocks = [
-   EduzzLoginBlock,
-   GoogleAnalyticsCreatePropertyBlock,
-   MyNutrorCreateModulesAndLessonsBlock,
-   EduzzCreateMultipleCouponsBlock,
-   JivoCreateChannelBlock,
    YTEvalDurationScraperConverterBlock
 ];
